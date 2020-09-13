7359359eaa4f4bc8ccb78800cf0bb225f31d9c9a
ZborovskiyA <zborovi4@gmail.com>
2020-09-14 01:49:25 +0300

    Добавлен новый контроллер Topic и реализован вывод всех сообщений выбранной темы + возможность добавлять в тему новое сообщение

---
+++ First_Forum/Controllers/TopicController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using First_Forum.Models;
+
+namespace First_Forum.Controllers
+{
+    public class TopicController : Controller
+    {
+        private ForumContext db = new ForumContext();
+
+        // GET: Topic
+        public async Task<ActionResult> Index(int? id)
+        {
+            var forum_posts = db.Forum_post.Select(p => new Post
+            { 
+                Id = p.Id,
+                Author = p.Author,
+                Topic = p.Topic,
+                Body = p.Body,
+                Date = p.Date,
+                Post_id = p.Post_id,
+                Topic_id = p.Topic_id,
+                Forum_id = p.Forum_id
+            }).Where(p => p.Topic_id == id);
+
+            TempData["forum"] = forum_posts.FirstOrDefault().Forum_id;
+            TempData["topic"] = id;
+            return View(await forum_posts.ToListAsync());
+        }
+        
+        // GET: Topic/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Topic/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Body")] Forum_post forum_post)
+        {
+            if (ModelState.IsValid)
+            {
+                forum_post.Author = "Lesya";
+                forum_post.Date = DateTime.Now;
+                forum_post.Forum_id = (int)TempData["forum"];
+                forum_post.Topic_id = (int)TempData["topic"];
+                forum_post.Post_id = db.Forum_post.Count(p => p.Topic_id == forum_post.Topic_id) + 1;
+
+                db.Forum_post.Add(forum_post);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(forum_post);
+        }
+
+        // GET: Topic/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Forum_post forum_post = await db.Forum_post.FindAsync(id);
+            if (forum_post == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Forum_id = new SelectList(db.Forum, "Forum_id", "Name", forum_post.Forum_id);
+            return View(forum_post);
+        }
+
+        // POST: Topic/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Author,Topic,Body,Date,Post_id,Topic_id,Forum_id")] Forum_post forum_post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(forum_post).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Forum_id = new SelectList(db.Forum, "Forum_id", "Name", forum_post.Forum_id);
+            return View(forum_post);
+        }
+
+        // GET: Topic/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Forum_post forum_post = await db.Forum_post.FindAsync(id);
+            if (forum_post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(forum_post);
+        }
+
+        // POST: Topic/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Forum_post forum_post = await db.Forum_post.FindAsync(id);
+            db.Forum_post.Remove(forum_post);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ First_Forum/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace First_Forum.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Author { get; set; }
+
+        public string Topic { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Post_id { get; set; }
+
+        public int Topic_id { get; set; }
+
+        public int Forum_id { get; set; }
+    }
+}
